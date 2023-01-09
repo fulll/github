90419443e94360a7b86a8c22c94def0588c449b1
vibou <vincent.bouvier1987@gmail.com>
2023-01-09 15:44:57 +0100

    fix setup

--- install-local.sh
+++ install-local.sh
@@ -88,7 +88,7 @@ checkGithubInstalledVersion() {
 installFile() {
   echo "Preparing to install $PROJECT_NAME from ${GH_CLI_TMP} ${GH_CLI_INSTALL_DIR}"
   chmod +x "${GH_CLI_TMP}"
-  runAsRoot cp "${GH_CLI_TMP}" "$GH_CLI_INSTALL_DIR"
+  runAsRoot cp "${GH_CLI_TMP}" "$GH_CLI_INSTALL_DIR/$PROJECT_NAME"
   ls $GH_CLI_INSTALL_DIR
   echo "$PROJECT_NAME installed into $GH_CLI_INSTALL_DIR/$PROJECT_NAME"
 }
